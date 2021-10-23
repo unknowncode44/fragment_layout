97dd0664026e61887178f92f054472320f86dda6
unknowncode44 <matiaz.orellana@gmail.com>
2021-10-23 19:10:55 -0300

    se vincula database firestore

--- app/src/main/java/com/example/landscapecontacts/ContactList.kt
+++ app/src/main/java/com/example/landscapecontacts/ContactList.kt
@@ -1,16 +1,20 @@
 package com.example.landscapecontacts
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 
 class ContactList : Fragment() {
 
+    val db = Firebase.firestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,12 +33,19 @@ class ContactList : Fragment() {
 
         listaDeContactos.setHasFixedSize(true)
 
+        readData()
+
         // LLAAMAR A FUNCION PARA OBTENER DATOS DE FIREBASE, QUE CREE EL ARRAY CON LOS OBJETOS Contact.
 
         return vista
     }
 
     // CREAR FUNCION PARA OBTENER DATOS DESDE FIREBASE
+
+    private fun readData() {
+
+
+    }
 
 
 }
