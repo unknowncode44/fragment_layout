da810f503a6dc94358b7cbe7173e4b593f1dddf1
unknowncode44 <matiaz.orellana@gmail.com>
2021-10-24 02:14:36 -0300

    se finaliza fragment ContactList.kt, logrando que se muestren los contactos en el recycler, se arregla un toke el layout (se desestima el uso del cardview por temas de diseno). Se agrega funcion para enviar los dats del contacto al otro fragment cuando se hace click. costo un huevo, son las 2 am.

--- app/src/main/java/com/example/landscapecontacts/ContactCardAdapter.kt
+++ app/src/main/java/com/example/landscapecontacts/ContactCardAdapter.kt
@@ -8,27 +8,52 @@ import android.view.ViewGroup
 
 class ContactCardAdapter(private val contactList: ArrayList<Contact>): RecyclerView.Adapter<ContactCardAdapter.ContactCardViewHolder>() {
 
+    // creamos este listener para escuchar luego si hacen un click en uno de los contactos, de esa manera recogeremos la data y la mandaremos al otro fragment
+    private lateinit var mListener: OnItemClickListener
+    interface OnItemClickListener {
+        fun onItemClick(position: Int, name: String, number: String, email: String) // definimos las variables que obtendremos al tocar el boton
+    }
+
+    // creamos la funcion para poder usarla luego en el fragment
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        mListener = listener
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactCardViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_card, parent, false)
-        return ContactCardViewHolder(view)
+        return ContactCardViewHolder(view, mListener)
     }
 
     override fun onBindViewHolder(holder: ContactCardViewHolder, position: Int) {
-        val currentItem = contactList[position]
+        val contact: Contact = contactList[position]
 
-        holder.contactName.text = currentItem.name
-        holder.contactNumber.text = currentItem.number
-        holder.contactEmail.text = currentItem.email
+
+        holder.name.text = contact.name
+        holder.number.text = contact.number
+        holder.email.text = contact.email
     }
+
+
+
 
     override fun getItemCount(): Int {
         return contactList.size
     }
 
-    class ContactCardViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        val contactName: TextView = itemView.findViewById(R.id.contact_name)
-        val contactNumber: TextView = itemView.findViewById(R.id.contact_number)
-        val contactEmail: TextView = itemView.findViewById(R.id.contact_email)
+    class ContactCardViewHolder(itemView: View, listener: OnItemClickListener): RecyclerView.ViewHolder(itemView) { // agregamos el listener a los parametros
+        val name: TextView = itemView.findViewById(R.id.contact_name)
+        val number: TextView = itemView.findViewById(R.id.contact_number)
+        val email: TextView = itemView.findViewById(R.id.contact_email)
+
+        // le pasamos los datos que recogimos al listener para tenerlos disponibles para pasarlos al otro fragment
+
+        init {
+            itemView.setOnClickListener {
+
+                listener.onItemClick(bindingAdapterPosition, name.text.toString(), number.text.toString(), email.text.toString())
+            }
+
+        }
 
     }
 }
--- app/src/main/java/com/example/landscapecontacts/ContactList.kt
+++ app/src/main/java/com/example/landscapecontacts/ContactList.kt
@@ -1,20 +1,28 @@
 package com.example.landscapecontacts
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.firebase.database.DatabaseReference
+import com.google.firebase.firestore.*
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 
 class ContactList : Fragment() {
 
-    val db = Firebase.firestore
+    private var db = Firebase.firestore
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var contactArrayList: ArrayList<Contact>
+    private lateinit var adapter: ContactCardAdapter
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,23 +35,64 @@ class ContactList : Fragment() {
     ): View {
 
         val vista: View =  inflater.inflate(R.layout.contact_list, container, false)
+        recyclerView = vista.findViewById(R.id.recycler_contacts)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.setHasFixedSize(true)
 
-        val listaDeContactos: RecyclerView = vista.findViewById(R.id.recycler_contacts)
+        contactArrayList = arrayListOf()
+
+        adapter = ContactCardAdapter(contactArrayList)
+
+        recyclerView.adapter = adapter
+
+        // con esta funcion deberiamos mandar los items al otro fragment
+
+        adapter.setOnItemClickListener(object : ContactCardAdapter.OnItemClickListener{
+            override fun onItemClick(position: Int, name: String, number: String, email: String) {
+
+                // TODO: 24/10/21 MANDAR LOS DATOS AL OTRO FRAGMENT, YA LOS TENEMOS!
 
 
-        listaDeContactos.setHasFixedSize(true)
+                Toast.makeText(context, "hiciste click en $position nombre $name, numero $number, email $email", Toast.LENGTH_LONG).show()
+            }
+
+        })
+
+
+
 
         readData()
 
-        // LLAAMAR A FUNCION PARA OBTENER DATOS DE FIREBASE, QUE CREE EL ARRAY CON LOS OBJETOS Contact.
 
         return vista
     }
 
     // CREAR FUNCION PARA OBTENER DATOS DESDE FIREBASE
-
     private fun readData() {
-
+        // primero obtenemos la instancia de la db
+        db = FirebaseFirestore.getInstance()
+        // luego apuntamos a la coleccion contacts donde estan guardados los contactos
+        db.collection("contacts").
+            addSnapshotListener(object : EventListener<QuerySnapshot>{ // creamos un listener para captar los nuevos contactos que agreguemos (tambien aplica para los que ya tenemos)
+                @SuppressLint("NotifyDataSetChanged")
+                override fun onEvent(
+                    value: QuerySnapshot?, // value sera igual al snapshot que hagamos
+                    error: FirebaseFirestoreException? // value sera igual al error que detecte
+                ) {
+                    if (error != null) { // si error es distinto de nulo, es decir que existe el error, lo mostraremos por consola
+                        Log.e("Error!!", error.message.toString())
+                        return // hacemos return void
+                    }
+                    // si no hay error hacemos un loop para agregar los datos al array
+                    for (dc: DocumentChange in value?.documentChanges!!){
+                        if (dc.type == DocumentChange.Type.ADDED){
+                            Log.d("DATABASE","SE ENCONTRARON DATOS!!")
+                            contactArrayList.add(dc.document.toObject(Contact::class.java)) //lo agregamos usando la clase que creamos para los contacts
+                        }
+                    }
+                    adapter.notifyDataSetChanged() // notificamos al adaptador para que cree las tarjetas
+                }
+            })
 
     }
 
